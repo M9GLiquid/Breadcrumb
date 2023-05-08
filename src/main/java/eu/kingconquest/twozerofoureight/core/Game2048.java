1131bf097334861d68b2d31087a18d9ba217b75c
Hieu Tran <hieu.tran19981998@gmail.com>
2023-05-08 15:08:03 +0200

    Started implementing the game 2048 and so far only the start menu.
    Using our created Framework.

--- src/main/java/eu/kingconquest/twozerofoureight/core/Game2048.java
+++ src/main/java/eu/kingconquest/twozerofoureight/core/Game2048.java
@@ -1,27 +1,49 @@
 package eu.kingconquest.twozerofoureight.core;
 
+import eu.kingconquest.framework.controllers.GameController;
 import eu.kingconquest.framework.core.Game;
+import eu.kingconquest.framework.core.GameState;
+import eu.kingconquest.framework.observers.StateObserver;
 import eu.kingconquest.framework.ui.GameFrame;
+import eu.kingconquest.framework.ui.StartMenu;
+import eu.kingconquest.framework.utils.Tile;
 
 public class Game2048 extends Game {
+    public Game2048(GameFrame gameframe) {
+        super(gameframe,"2048");
 
-    public Game2048(GameFrame gameFrame) {
-        super(gameFrame, "2048");
+        // Game Board setup
+        board = new Board2048(this, null, null);
+        board.setState(GameState.RUNNING);
+
+        // Game Controller setup
+        controller = new GameController(board);
+
+        // Key Listener setup
+        getGameFrame().addKeyListener(getController());
+
+        // Game View setup
+        getGameFrame().addView(new StartMenu(this), 970, 640);
+
+        // Game Observers Setup
+        controller.addObserver(new StateObserver(this));
+        controller.notifyObservers();
+
+        Tile.setTileSize(64);
     }
 
     @Override
     public void initiate() {
-
+        getBoard().setState(GameState.INITIATING);
     }
 
     @Override
     public void gameOver() {
-
+        getBoard().setState(GameState.GAME_OVER);
     }
 
     @Override
     public void start() {
-
     }
 
     @Override

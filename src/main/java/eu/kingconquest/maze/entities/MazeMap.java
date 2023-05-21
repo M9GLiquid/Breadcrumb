f2aeb8f586ff0f3718aa7d233f612a0ab7765c03
rayye <rim.abdennour02@gmail.com>
2023-05-21 12:04:37 +0200

    Fixes?
    Signed-off-by: rayye <rim.abdennour02@gmail.com>

--- src/main/java/eu/kingconquest/maze/entities/MazeMap.java
+++
@@ -1,9 +0,0 @@
-package eu.kingconquest.maze.entities;
-
-import eu.kingconquest.framework.utils.Location;
-
-public class MazeMap {
-//    public MazeMap(Location location){super(location, MazeEntityIcon.WALL);}
-
-
-}
